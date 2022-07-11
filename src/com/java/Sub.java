4fcee79ab6b6247c8f524906b7d77ad7cc39820d
Punam <punamkharche28@gmail.com>
2022-07-11 15:13:22 +0530

    I add some code into sub.java

--- src/com/java/Sub.java
+++ src/com/java/Sub.java
@@ -3,8 +3,9 @@ package com.java;
 public class Sub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a =35-5;
+		System.out.println("THe value of c= "+c);
 	}
 
 }
